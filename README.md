5cb8cfd2065887d0ac88273531f91ed348298ceb
aashnajhaveri01@gmail.com <aashna@c02c4069lvdl.myfiosgateway.com>
2023-01-10 11:18:22 -0500

    added reference file link in the job submission script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # multi-Omics
+
+This repository is built to run it on kraken server.
+
+Please use wes.sh to submit the jobs after the config file and metasheet is ready.
--- wes.sh
+++ wes.sh
@@ -5,16 +5,20 @@
 #SBATCH --time=72:00:00
 
 
-##Setting up the sentieon license
+##Export up the sentieon license
 export SENTIEON_LICENSE=/michorlab/aashna/WES/sentieon/Harvard_DFCI_Iris.lic
 
+##Activate the environment
 CONDA_PREFIX=/michorlab/jacobg/miniconda3
 export CONDA_ROOT=/michorlab/jacobg/miniconda3
 export PATH=/michorlab/jacobg/miniconda3/bin:$PATH
-
 source activate wes
 module load samtools
 module load bcftools
 
+##Link the Reference files
+ln -s /michorlab/aashna/multiomics/wes/reference_files
+
+##Run the snakemake command
 snakemake --unlock -s wes/wes.snakefile -j 32 -k
 snakemake --rerun-incomplete  -s wes/wes.snakefile -j 32 -k
